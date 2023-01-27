f21ad6123e8bc6fb2852f5bd3d11dedc3dc30312
ajeer333 <ajeer333@gmail.com>
2023-01-27 14:48:42 +0100

    commit

--- js/album-page.js
+++ js/album-page.js
@@ -52,6 +52,7 @@ const getAlbum= async (id)=>{
   let res= await fetch(url+id,{options})
   let artist= await res.json()
   console.log(artist)
+
  if(res.ok){
     let time=0
       
@@ -79,18 +80,25 @@ const getAlbum= async (id)=>{
         //     console.log(music[0])
         //   }
         //  }
-   
+ 
 
         searchQuery=track.artist.id;
         tr.innerHTML=`
         <th scope="row" class="play-list-text">${i+1}</th>
-        <td class="white-text no-space-left">${track.title}
-       <a href="artist.html?id=${searchQuery}"><p class="play-list-text">${track.artist.name}</p></a></td>
+        <td class="white-text no-space-left">
+        <div class="d-flex">
+        <img class="table-image" src=${track.album.cover_small}>
+      <div class="d-flex flex-column"> <span> ${track.title}</span>
+       <a href="artist.html?id=${searchQuery}"><p class="play-list-text light-text">${track.artist.name}</p></a>
+       </div>
+       </div>
+       </td>
         <td></td>
         <td class="play-list-text">${mins}</td>
 
         `
-    
+        
+
       
         console.log(track.preview.duration)
     
@@ -126,12 +134,18 @@ const getAlbum= async (id)=>{
   }catch(err){
     console.log(err)
   }
+  likeBtn()
 }
 
  
 
-
-
+function likeBtn(){
+  let node=document.querySelector('svg:nth-of-type(1)')
+  node.addEventListener("click",()=>{
+    let path=node.childNodes[1]
+    path.setAttribute("d","M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z")
+  })
+}
 
 window.onload=getAlbum(id)
 
